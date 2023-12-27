324c0e91799d1b86d889aa56193fbd0b70425af9
Sseyder <leonel.marsilli.123@gmail.com>
2023-12-27 11:56:35 -0300

    Cambios menores

--- Day4/ejercicios.js
+++ Day4/ejercicios.js
@@ -1,3 +1,5 @@
+/*
+
 // Ejercicio 1:
 //Get user input using prompt(“Enter your age:”).
 //If user is 18 or older , give feedback:'You are old enough to drive' 
@@ -63,8 +65,77 @@ console.log(comparacion);
 let introducirNumero = prompt(`Introduzca su numero:`);
 let miNumero = parseInt(introducirNumero);
 // Ver si el numero es par o no
-if (myNumber % 2 === 0) {
-    console.log(`${myNumber} is an even number.`);
+if (miNumero % 2 === 0) {
+    console.log(`${miNumero} is an even number.`);
 } else {
-    console.log(`${myNumber} is an odd number.`);
+    console.log(`${miNumero} is an odd number.`);
 };
+
+*/
+
+// Ejercicio 4:
+// Write a code which can give grades to students according to theirs scores:
+// 80-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+function calculateGrade(score) {
+    if (score >= 80 && score <= 100) {
+        return 'A';
+    } else if (score >= 70 && score < 80) {
+        return 'B';
+    } else if (score >= 60 && score < 70) {
+        return 'C';
+    } else if (score >= 50 && score < 60) {
+        return 'D';
+    } else if (score >= 0 && score < 50) {
+        return 'F';
+    } else {
+        return 'Invalid Score';
+    }
+}
+// Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+function determineSeason(month) {
+    if (['Septiembre', 'Octubre', 'Noviembre'].includes(month)) {
+        return 'Primavera';
+    } else if (['Diciembre', 'Enero', 'Febrero'].includes(month)) {
+        return 'Verano';
+    } else if (['Marzo', 'Abril', 'Mayo'].includes(month)) {
+        return 'Otoño';
+    } else if (['Junio', 'Julio', 'Agosto'].includes(month)) {
+        return 'Invierno';
+    } else {
+        return 'Invalid Month';
+    };
+};
+// Check if a day is weekend day or a working day.
+function checkWorkingDay(day) {
+    if (['Sabado', 'Domingo'].includes(day)) {
+        return 'Weekend';
+    } else if (['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'].includes(day)) {
+        return 'Working Day';
+    } else {
+        return 'Invalid Day';
+    };
+};
+
+let userScore = prompt("Ingrese su nota de examen:");
+let userMonth = prompt("Ingrese el mes:");
+let userDay = prompt("Ingrese el día de la semana:");
+
+let score = parseInt(userScore);
+let month = userMonth.trim();
+let day = userDay.trim();
+
+let grade = calculateGrade(score);
+let season = determineSeason(month);
+let dayType = checkWorkingDay(day);
+
+console.log(`Nota: ${grade}`);
+console.log(`Estación: ${season}`);
+console.log(`Dia laboral: ${dayType}`);
